0d58bc00cb924eeb667f63d2494dbb070966b6f1
Linda <linda.dogan@outlook.fr>
2020-07-17 17:39:24 +0200

    Adding validators for each field

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,68 @@
 module.exports = mongoose => {
   var userSchema = mongoose.Schema(
       {
-        firstName: String,
-        lastName: String,
-        email: String,
-        phoneNumber: String,
-        tag: String
+        firstName: {
+          type: String,
+          required: true,
+          lowercase: true,
+          trim: true,
+          minLenght: [3, "Firstname is too short"],
+          maxLength: 15
+        },
+        lastName: {
+          type: String,
+          required: true,
+          lowercase: true,
+          trim: true,
+          minLenght: [3, "Lastname is too short"],
+          maxLength: 15
+        },
+        email: {
+          type: String,
+          required: true,
+          lowercase: true,
+          trim: true
+        },
+        phoneNumber: {
+          type: String,
+          required: true,
+          lowercase: true,
+          trim: true
+        },
+        tag: {
+          type: String,
+          required: true,
+          lowercase: true,
+          trim: true
+        }
       },
       { timestamps: true }
   );
+
+  userSchema.path('firstName').validate(function (firstName) {
+    var stringRegex = /[a-z]/;
+    return stringRegex.test(firstName);
+  },'The Firstname field is not in the correct format.')
+
+  userSchema.path('lastName').validate(function (lastName) {
+    var stringRegex = /[a-z]/;
+    return stringRegex.test(lastName);
+  },'The Lastname field is not in the correct format.')
+  
+  userSchema.path('phoneNumber').validate(function (phoneNumber) {
+    var stringRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return stringRegex.test(phoneNumber);
+  },'Your phone number field is not in the correct format.')
+  
+  userSchema.path('email').validate(function (email) {
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email);
+ }, 'The email field is not in the correct format.')
+
+ userSchema.path('tag').validate(function (tag) {
+  var stringRegex = /[a-z]/;
+  return stringRegex.test(tag);
+},'The tag field is not in the correct format.')
 
 // Transform _id by id for the front-end 
   userSchema.method("toJSON", function() {
